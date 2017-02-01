9f64957f2637aefc56f7a813ef7ae1ace78b5f58
Alexey Ruzin <ruzin@me.com>
2017-02-02 01:45:54 +0300

    'diff'

--- lesson-6/answers.py
+++ lesson-6/answers.py
@@ -39,4 +39,58 @@ def log(filename):
 def something_wrong(*args, **kwargs):
     raise Exception('something goes wrong with {} and {}'.format(args, kwargs))
 
-something_wrong(1, 2, key=34)
+# something_wrong(1, 2, key=34)
+
+
+# Задача 3
+# Написать аналог команды diff (построчное сравнение двух файлов).
+
+def diff(filename1, filename2):
+    with open(filename1, 'r') as f1, open(filename2, 'r') as f2:
+        buf1 = []
+        buf2 = []
+        eof1 = False
+        eof2 = False
+
+        def readline(file, buf):
+            line = file.readline()
+            if line:
+                buf.append(line)
+            return line, line == ''
+
+        def dumpbufs(b1, b2, i1, i2):
+            for l in b1[:i1-1]:
+                print("> {}".format(l), end="")
+            for l in b2[:i2-1]:
+                print("< {}".format(l), end="")
+            return b1[i1:], b2[i2:]
+
+        while True:
+            line1, eof1 = readline(f1, buf1)
+            line2, eof2 = readline(f2, buf2)
+            if eof1 and eof2:
+                buf1, buf2 = dumpbufs(buf1, buf2, len(buf1), len(buf2))
+                break
+
+            for i in range(max(len(buf1), len(buf2))):
+                if i < len(buf2) and line1 == buf2[i]:
+                    buf1, buf2 = dumpbufs(buf1, buf2, len(buf1), i+1)
+                if i < len(buf1) and line2 == buf1[i]:
+                    buf1, buf2 = dumpbufs(buf1, buf2, i+1, len(buf2))
+
+diff('b.txt', 'a.txt')
+
+
+# Задача 4
+# Написать программу аналогичную tail, которая выводит
+# последние 10 строк текстового файла, переданного на stdin
+
+import sys
+buf = []
+for line in sys.stdin:
+    buf.append(line)
+    while len(buf) > 10:
+        del buf[0]
+
+for line in buf:
+    print(line, end="")

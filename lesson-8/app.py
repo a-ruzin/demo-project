ee4899138970dcf07d8a4ea6baf8ae1ccd8fba43
Alexey Ruzin <ruzin@kokoc.com>
2017-02-07 02:22:38 +0300

    занятие 8

---
+++ lesson-8/app.py
@@ -0,0 +1,19 @@
+import shutil
+
+f1 = open('app.py', 'r')  # русский комментарий
+f2 = open('app2.py', 'w')
+
+print(f1.tell())
+f1.seek(3)
+print(f1.read(30))
+print(f1.tell())
+print(f1.readline())
+print(f1.tell())
+
+import os
+
+for dirname, dirs, files in os.walk('..'):
+    print(dirname, dirs, files)
+
+os.remove('app2.py')
+shutil.rmtree('test')
